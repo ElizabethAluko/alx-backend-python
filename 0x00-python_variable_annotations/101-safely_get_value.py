4a334a5e10f037db446f737210bfdba41b9e10d3
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 21:36:19 +0100

    Add task12

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate iterable object  annotations"""
+from typing import TypeVar, Mapping, Any, Union
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Safely get the value associated with the given key from the
+    dictionary.
+
+    Parameters:
+        dct (Mapping): The input dictionary.
+        key (Any): The key to search in the dictionary.
+        default (Optional[T]): The default value to return if the
+        key is not found (default is None).
+
+    Returns:
+        Union[Any, T]: The value associated with the key in the
+        dictionary if found, else the default value.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
