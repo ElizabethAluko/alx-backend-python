ae00b0a7a0a1c9100e4a3a2fa9a7503802ca9ff7
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 21:39:08 +0100

    Add task12

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -2,6 +2,7 @@
 """Contains a function to demostrate iterable object  annotations"""
 from typing import TypeVar, Mapping, Any, Union
 
+
 T = TypeVar('T')
 
 
@@ -20,6 +21,7 @@ def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> 
         Union[Any, T]: The value associated with the key in the
         dictionary if found, else the default value.
     """
+
     if key in dct:
         return dct[key]
     else:
