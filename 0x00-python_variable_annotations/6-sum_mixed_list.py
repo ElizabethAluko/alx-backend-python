f2a885e3a0c67150cc248a7bd664f57c22b62c50
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 17:50:10 +0100

    Add task6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate annotations - mixed lists"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[float | int]) -> float:
+    """
+    Add the lists items of a list
+
+    parameters:
+    mxd_lst: the list to sum its items.
+
+    return:
+        float: sum of the items
+    """
+    return sum(mxd_lst)
