f2c784336850b8af628f006f846cefe7574ffe93
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 22:11:45 +0100

    Add task12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Check type annotation using mypy"""
-from typing import Tuple
+from typing import Tuple, List
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> Tuple[int, ...]:
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
     """Debugging"""
 
-    zoomed_in: Tuple[int, ...] = tuple(
+    zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
-    )
+    ]
     return zoomed_in
 
 
-array = (12, 72, 91,)
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
