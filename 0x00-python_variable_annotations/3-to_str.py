3a99d274287847776982c52c6d025699745f6dce
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 16:21:39 +0100

    Add task3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate basic annotations - to string"""
+
+
+def to_string(n: float) -> str:
+    """
+    Convert a float number to string
+    n: float number to convert to string
+    """
+    return str(n)
