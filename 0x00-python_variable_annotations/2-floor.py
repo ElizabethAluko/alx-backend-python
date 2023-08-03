b4677a479f0f9bd9905ece53659c39de2e33696c
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 16:12:28 +0100

    Add task2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate basic annotations - floor"""
+
+
+def floor(n: float) -> int:
+    """
+    returns the floor of a numbet
+    n: float number to get its floor value
+    """
+    return(n)
