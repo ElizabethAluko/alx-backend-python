967700e99452401c08e6cb9a848e41db949b59de
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 20:43:47 +0100

    Add task6

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate annotations - complex types"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Takes a float multiplier as an argument and returns a function that multiplies a float by multiplier.
+
+    Parameters:
+        multiplier (float): The multiplier value.
+
+    Returns:
+        Callable[[float], float]: The function that multiplies a float by the multiplier.
+    """
+    def multiply(value: float) -> float:
+        return value * multiplier
+
+    return multiply
+
