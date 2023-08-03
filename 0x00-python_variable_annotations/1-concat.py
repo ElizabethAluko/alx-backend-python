89c1bb8c8e4b5c6ebe67b5dc7872c5fbde4f1921
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 16:04:58 +0100

    Add task1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    join two given strings
+    str1: first string.
+    str2: second string.
+    """
+    return(str1 + str2)
