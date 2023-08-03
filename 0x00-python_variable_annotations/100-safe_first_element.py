a023558c2be28750d43879aeea5deefaa82c9377
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 21:23:16 +0100

    Add task10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate iterable object  annotations"""
+from typing import Union, Sequence
+
+
+# The types of the elements of the input are not know
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+    """Take sequence of any type and return Any type or none"""
+    if lst:
+        return lst[0]
+    else:
+        return None
