59ca0f4a1014a28f1f0e4c0bc9a1cbfb4d496ce1
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 17:44:26 +0100

    Add task5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """Contains a function to demostrate annotations - lists of float"""
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Add the lists items of a list
-    input_list: the list to sum its items.
-    """
-    sum_of_list: float = 0.0
 
-    for i in range(len(input_list)):
-        sum_of_list += i
-    return (sum_of_list)
+    parameters:
+    input_list: the list to sum its items.
+
+    return:
+        float: sum of the items
+    """
+    return sum(input_list)
