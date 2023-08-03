f50c4382375c32f8efb7fa515f33fe1caf4bcceb
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 18:41:47 +0100

    Add task7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate annotations - complex types"""
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Taka a string and an int/float and return a tuple.
+
+    parameters:
+    k: a string.
+    v: int or float to be squared.
+
+    Return:
+        tuple: of k and square of v
+    """
+    return tuple(k, float(v) ** 2))
