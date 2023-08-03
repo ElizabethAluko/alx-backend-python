44ccc2ffa2a1eb7c26d73e0e7dd94c53ec01d5b1
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 22:29:54 +0100

    Add task12

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -3,10 +3,10 @@
 from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """Function was checked for type hint and corrected"""
 
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
