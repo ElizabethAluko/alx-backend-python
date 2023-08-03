35725d22de4c291708df19616cddefee5ed6269f
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-03 15:59:36 +0100

    Add README file and task0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Contains a function to demostrate basic annotations"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add two given float numbers
+    a: first integer.
+    b: second integer
+    """
+    return(a + b)
