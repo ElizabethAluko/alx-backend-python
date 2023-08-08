2b7fec91b19934139f77614524bb67766037c0cb
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-08 13:44:10 +0100

    Add task1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,5 +9,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> Generator[float, None, None]:
     """Function returns 10 random numbers"""
+
     random_numbers = [number async for number in async_generator()]
     return random_numbers
