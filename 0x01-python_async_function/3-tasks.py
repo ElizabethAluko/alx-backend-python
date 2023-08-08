fc3e6c13efa67cbb071da9d83b1f40e892b84e63
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-08 07:44:17 +0100

    Debug task3

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Task 3"""
+"""The module tasks an integer and returns asyncio.Task"""
 
 import asyncio
 from typing import Coroutine
 
-# Importing wait_random from 0-basic_async_syntax.py
 wait_random = __import__('0-basic_async_syntax').wait_random
 
-def task_wait_random(max_delay: int) -> asyncio.Task:
+
+def task_wait_random(max_delay: int) -> asyncio.Taski:
+    """Function take an integer and return asyncio.Task"""
     loop = asyncio.get_event_loop()
     task = loop.create_task(wait_random(max_delay))
     return task
