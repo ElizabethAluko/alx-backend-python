7ea9574fdebe074825b342f95b2c16bfebf7ff25
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-08 07:37:23 +0100

    Debug task2

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 """Contain delay module that Import wait_random"""
+
 import asyncio
-"""from typing import callable"""
 import time
 
-wait_n = __import__ ('1-concurrent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
