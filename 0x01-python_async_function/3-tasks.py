f2bb45bc78cdc9bb4b84a93686e0ee1d14e497fe
ElizabethAluko <jolaoyebeth24@gmail.com>
2023-08-08 05:59:29 +0100

    Add task3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Task 3"""
+
+import asyncio
+from typing import Coroutine
+
+# Importing wait_random from 0-basic_async_syntax.py
+from 0-basic_async_syntax import wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(wait_random(max_delay))
+    return task
